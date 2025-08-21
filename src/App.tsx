77ba87244ed7cb49b74e5ba6892599b36f466a0e
bruno <bruno.b.fernandes12@hotmail.com>
2025-08-21 19:14:55 -0300

    feat: set up Redux store and initial cart slice  - configure store and add reducer with add action

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import Header from "./components/Header";
+import { Provider } from "react-redux";
 import { GlobalStyle } from "./styles";
 
 import Footer from "./components/Footer";
 import { BrowserRouter } from "react-router-dom";
 import Rotas from "./routes";
+import { store } from "./store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Rotas />
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Rotas />
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
